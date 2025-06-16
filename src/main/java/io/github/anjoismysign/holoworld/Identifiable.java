387b5fa7de1bb2f70d507afd6b2f2810834cda7b
Luis Benavides Naranjo <lubensaico@gmail.com>
2025-06-15 21:29:57 -0600

    refactor: extend Identifiable interface to support comparison

--- src/main/java/io/github/anjoismysign/holoworld/Identifiable.java
+++ src/main/java/io/github/anjoismysign/holoworld/Identifiable.java
@@ -2,7 +2,13 @@ package io.github.anjoismysign.holoworld;
 
 import org.jetbrains.annotations.NotNull;
 
-public interface Identifiable {
+public interface Identifiable extends Comparable<Identifiable> {
     @NotNull
     String identifier();
+
+    @Override
+    default int compareTo(@NotNull Identifiable other) {
+        return identifier().compareTo(other.identifier());
+    }
+
 }
